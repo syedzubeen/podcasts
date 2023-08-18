3b6ca0d197ac0d267a84bfacbe28ff710a810d76
syedzubeen <zubeenqadry@gmail.com>
2023-08-19 03:29:48 +0530

    Added firebase support, Customised splash screen, Formatted list elements containing podcasts

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:xml/xml.dart' as xml;
 import 'package:just_audio/just_audio.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:flutter/services.dart'; // Import for SystemChrome
 
-void main() {
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Customize the status bar
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.blue, // Set the status bar color
+      statusBarBrightness: Brightness.dark, // Set the status bar content color
+    ));
     return MaterialApp(
       title: 'TED Talks Podcasts',
+
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black
+        ),
+      ),
       home: PodcastListScreen(),
     );
   }
@@ -81,12 +100,46 @@ class _PodcastListScreenState extends State<PodcastListScreen> {
     });
   }
 
+  Future<void> downloadAndUploadToFirebaseStorage(PodcastItem podcast) async {
+    final audioUrl = podcast.audioUrl;
+    if (audioUrl != null) {
+      final response = await http.get(Uri.parse(audioUrl));
+      if (response.statusCode == 200) {
+        final audioBytes = response.bodyBytes;
+
+        // Upload the audio file to Firebase Storage
+        final storageReference = firebase_storage.FirebaseStorage.instance
+            .ref()
+            .child('podcasts/${DateTime.now().millisecondsSinceEpoch}.mp3');
+
+        final uploadTask = storageReference.putData(audioBytes);
+        final snapshot = await uploadTask.whenComplete(() {});
+
+        // Get the download URL
+        if (snapshot.state == firebase_storage.TaskState.success) {
+          final downloadUrl = await snapshot.ref.getDownloadURL();
+          print('Audio uploaded to Firebase Storage: $downloadUrl');
+          // Here you can provide a UI indication that the download and upload were successful
+        } else {
+          print('Error uploading audio to Firebase Storage');
+          // Handle the error condition
+        }
+      } else {
+        print('Error downloading audio file');
+        // Handle the error condition
+      }
+    } else {
+      print('Audio URL is null');
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TED Talks Podcasts'),
+        title: Text('Podcasts.AI'),
+        backgroundColor: Colors.black,
       ),
       body: Column(
         children: [
@@ -95,9 +148,19 @@ class _PodcastListScreenState extends State<PodcastListScreen> {
               itemCount: podcasts.length,
               itemBuilder: (context, index) {
                 final podcast = podcasts[index];
-                return ListTile(
-                  title: Text(podcast.title),
-                  onTap: () => playPodcast(podcast),
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Text(podcast.title),
+                      trailing: IconButton(
+                        icon: Icon(Icons.download_for_offline_outlined),
+                        onPressed: () => downloadAndUploadToFirebaseStorage(podcast),
+                        color: Colors.black, // Change this color to the desired color
+                      ),
+                      onTap: () => playPodcast(podcast),
+                    ),
+                    Divider(), // Add a Divider after each ListTile
+                  ],
                 );
               },
             ),
